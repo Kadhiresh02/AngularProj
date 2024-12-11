e633cb2ddc4257fae47b303c446bf177c2501fbe
Kadhiresh S <kadhiresh.s>
2024-12-11 18:52:14 +0530

    add

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { DirectiveComponent } from './directive/directive.component';
+import { ToggleImageComponent } from './toggle-image/toggle-image.component';
+import { MultiImageToggleComponent } from './multi-image-toggle/multi-image-toggle.component';
+import { ParentComponent } from './parent/parent.component';
+import { MypipesComponent } from './mypipes/mypipes.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,DatabindingComponent,DirectiveComponent,ToggleImageComponent,MultiImageToggleComponent,ParentComponent,MypipesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angularproj';
+}
---
+++ src/app/forms/rtf/rtf.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-rtf',
+  imports: [RouterOutlet],
+  templateUrl: './rtf.component.html',
+  styleUrl: './rtf.component.css'
+})
+export class RtfComponent {
+
+}
